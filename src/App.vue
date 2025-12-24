630f7ec58278dad93d2c698af888052398fd97e3
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-12-24 13:54:42 +0900

    feat: Add Firebase integration, refactor App.vue with VueUse and Tailwind, and include a basic test.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { useMouse } from '@vueuse/core'
+
+const { x, y } = useMouse()
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center space-y-4">
+    <h1 class="text-4xl font-bold text-blue-500">
+      Vite + Vue 3 + Tailwind + VueUse
+    </h1>
+    <p class="text-xl">
+      Mouse position: {{ x }}, {{ y }}
+    </p>
+    <div class="p-4 bg-gray-800 rounded-lg shadow-lg">
+      <p class="text-green-400">Tailwind CSS is working!</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
